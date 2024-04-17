e662a0a2a98efe5a95d45844e51bef3b2fb085cf
Charles <163931020+CharlesMagico@users.noreply.github.com>
2024-04-17 13:49:09 -0400

    Update community.sol

--- community.sol
+++ community.sol
@@ -46,7 +46,7 @@ abstract contract Context {
 abstract contract Ownable is Context {
     address private _owner;
 
-    string websiteLink = "https://InIOSE.com";
+    string public websiteLink = "https://InIOSE.com";
 
     /**
      * @dev Updates the websiteLink string with a new value
