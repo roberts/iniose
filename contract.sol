0a45b3c624b348f0387cfbc16af281f6d25865dc
Drew Roberts <github@drewroberts.com>
2024-04-17 18:41:58 -0400

    Add iniose ai marketing splitter ca

--- contract.sol
+++ contract.sol
@@ -1233,7 +1233,7 @@ contract iniose is ERC20, Ownable {
             developerLopsidedSellTax;
 
         communityWallet = address(0xF659b39772Af07be28F18d4DDef31508dbDFfB30); // Community Funds
-        marketingWallet = address(0x5b8b39653792cd58157334df815eba0c318c9ac8); // Marketing Funds
+        marketingWallet = address(0x66d78e9bf632aac1de394a2aacf29f7a7acd931d); // Marketing Funds
         developerWallet = address(0xEB6601AB897119aa3e5fDF16ac967CCF959f364d); // Developer Funds
 
         _mint(address(this), totalSupply);
